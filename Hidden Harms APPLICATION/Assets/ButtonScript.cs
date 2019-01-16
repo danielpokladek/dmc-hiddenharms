53499c035fe88480aaf3258de6171e641a70b701
Daniel Pokladek <danielpokladek@gmail.com>
2019-01-16 13:45:12 +0000

    George's App & Documents
    Documents done by George before starting the project.

---
+++ Hidden Harms APPLICATION/Assets/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+    public GameObject DataManager;
+
+    public Button button;
+
+	// Use this for initialization
+	void Start () {
+        DataManager.gameObject.GetComponent<SendData>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (DataManager.gameObject.GetComponent<SendData>().AgeQs == true)
+        {
+            if (button.CompareTag("AGE"))
+            {
+                button.gameObject.SetActive(true);
+                
+
+            } else {
+                button.gameObject.SetActive(false);
+                
+            }
+        }
+
+	}
+}
