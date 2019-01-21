38700271afd0f971a3ac47420f579b1a29129444
Daniel Pokladek <danielpokladek@gmail.com>
2019-01-21 18:15:11 +0000

    Refined WWWForm code.
    Refined the code used to add fields, and send data to the online form. The new script "WebForm.cs" makes it easier to send data to a form.
    
    AddField - works in the same way as before, but now can be accessed from outside the script. New fields can be added on the go by any other script that has access to this one.
    
    ClearForm - removes all the field from the form.
    
    SendForm - loops through all the fields in the dictionary, adds them to the form and sends them to the web server. The web address needs to be passed as a variable.
    
    DisplayFormFields - loops through all the fields in the dictionary, and display the key + value.

---
+++ Hidden Harms APPLICATION/Assets/Scripts/WebForm.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebForm : MonoBehaviour {
+
+    WWWForm webForm;
+    Dictionary<string, string> formFields = new Dictionary<string, string>();
+
+    // Use this for initialization
+    void Start() {
+        webForm = new WWWForm();
+        SendTestData();
+    }
+
+    /// <summary>
+    /// Add a field to the web form. All fields are stored in a dictionary.
+    /// </summary>
+    /// <param name="fieldName">Field name, as seen on the web form. (Different for each field, check docs).</param>
+    /// <param name="value">Value of the field, given as a string.</param>
+    void AddField(string fieldName, string value)
+    {
+        formFields.Add(fieldName, value);
+    }
+
+    /// <summary>
+    /// Remove all fields from the form.
+    /// </summary>
+    void ClearForm()
+    {
+        formFields.Clear();
+    }
+
+    /// <summary>
+    /// Send form to the server. Returns a boolean.
+    /// </summary>
+    /// <param name="wwwAddress">WWW address for the form.</param>
+    bool SendForm(string wwwAddress)
+    {
+        if (formFields.Count == 0)
+        {
+            Debug.LogError("SEND ERR: No fields in the form.");
+            return false;
+        }
+
+        foreach (KeyValuePair<string, string> entry in formFields)
+        {
+            if (entry.Key == "" || entry.Value == "")
+            {
+                Debug.LogError("SEND ERR: Empty key/value in form at: " + entry.Key.ToString());
+                return false;
+            }
+            webForm.AddField(entry.Key, entry.Value);
+        }
+
+        byte[] rawData = webForm.data;
+        WWW formAddress = new WWW(wwwAddress, rawData);
+
+        if (formAddress == null)
+        {
+            Debug.LogError("SEND ERR: Could not access specified form page. Check the WWW address.");
+            return false;
+        }
+
+        Debug.Log("Form Sent. WWW Address: " + wwwAddress);
+        return true;
+    }
+
+    /// <summary>
+    /// Display form fields in console.
+    /// </summary>
+    void DisplayFormFields()
+    {
+        foreach (KeyValuePair<string, string> entry in formFields)
+        {
+            Debug.Log(entry.Key + " : " + entry.Value);
+        }
+    }
+
+    void SendTestData()
+    {
+        AddField("entry.416226614", "0");
+        AddField("entry.200936802", "Rather Not Say");
+        AddField("entry.679332033", "English");
+        AddField("entry.199891947", "English");
+        AddField("entry.738434021", "Nothing");
+        AddField("entry.711406450", "Nothing");
+
+        DisplayFormFields();
+
+        SendForm("https://docs.google.com/forms/d/e/1FAIpQLSeldUHslfO4JDGkjwX0Pjzwqu9Va5unoqYF0BTfSVnZwBRTzw/formResponse");
+    }
+}
