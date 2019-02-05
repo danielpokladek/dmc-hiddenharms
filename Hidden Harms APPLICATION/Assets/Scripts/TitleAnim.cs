7dade3866515a43e88c44603998a7d3762859e14
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2019-02-05 16:43:34 +0000

    Added change scene function and edited data collection screen
    Title

--- Hidden Harms APPLICATION/Assets/Scripts/TitleAnim.cs
+++ Hidden Harms APPLICATION/Assets/Scripts/TitleAnim.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleAnim : MonoBehaviour
 {
     public Animation anim;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,16 @@ public class TitleAnim : MonoBehaviour
 
     private void OnMouseDown()
     {
+
         anim.Play();
+        
+
+    }
+
+    public void ChangeScene()
+    {
+
+        SceneManager.LoadScene(0);
+
     }
 }
