44fc9a71f893aa91e4b9c4df290aab14669109ca
Daniel Pokladek <danielpokladek@gmail.com>
2019-04-22 18:40:57 +0100

    See notes below.
    * Added Debrief scene (needs contact info added)
    * Added Disclaimer at the start of the app.
    * Added full Polish translation of the app.
    * Added full Arabic translation of the app.

---
+++ Hidden Harms APPLICATION/Assets/Scripts/Certificate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Certificate : MonoBehaviour
+{
+    public Scene sceneToLoad;
+
+    public void BTN_Certificate()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
